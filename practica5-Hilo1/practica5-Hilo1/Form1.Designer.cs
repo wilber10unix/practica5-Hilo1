6500930558d4c05ac4764274b5368118dd7a27ac
Wilber manzanarez <55360108+wilber10unix@users.noreply.github.com>
2020-11-17 22:49:49 -0600

    Add files via upload
    Hilos

---
+++ practica5-Hilo1/practica5-Hilo1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace practica5_Hilo1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsdtipoletra = new System.Windows.Forms.ToolStripDropDownButton();
+            this.wideeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.arialToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tstamañoletra = new System.Windows.Forms.ToolStripComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ncaracteres = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nfilas = new System.Windows.Forms.Label();
+            this.text = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 303);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(771, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsdtipoletra,
+            this.tstamañoletra});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(771, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsdtipoletra
+            // 
+            this.tsdtipoletra.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tsdtipoletra.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wideeToolStripMenuItem,
+            this.arialToolStripMenuItem});
+            this.tsdtipoletra.Image = ((System.Drawing.Image)(resources.GetObject("tsdtipoletra.Image")));
+            this.tsdtipoletra.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsdtipoletra.Name = "tsdtipoletra";
+            this.tsdtipoletra.Size = new System.Drawing.Size(164, 22);
+            this.tsdtipoletra.Text = "toolStripDropDownButton1";
+            this.tsdtipoletra.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // wideeToolStripMenuItem
+            // 
+            this.wideeToolStripMenuItem.Name = "wideeToolStripMenuItem";
+            this.wideeToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.wideeToolStripMenuItem.Text = "Wide Latin";
+            this.wideeToolStripMenuItem.Click += new System.EventHandler(this.wideeToolStripMenuItem_Click);
+            // 
+            // arialToolStripMenuItem
+            // 
+            this.arialToolStripMenuItem.Name = "arialToolStripMenuItem";
+            this.arialToolStripMenuItem.Size = new System.Drawing.Size(130, 22);
+            this.arialToolStripMenuItem.Text = "Arial";
+            this.arialToolStripMenuItem.Click += new System.EventHandler(this.arialToolStripMenuItem_Click);
+            // 
+            // tstamañoletra
+            // 
+            this.tstamañoletra.Items.AddRange(new object[] {
+            "10",
+            "12",
+            "14",
+            "16",
+            "18",
+            "20",
+            "22",
+            "24",
+            "26",
+            "28",
+            "30"});
+            this.tstamañoletra.Name = "tstamañoletra";
+            this.tstamañoletra.Size = new System.Drawing.Size(75, 25);
+            this.tstamañoletra.SelectedIndexChanged += new System.EventHandler(this.tstamañoletra_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Caracteres:";
+            // 
+            // ncaracteres
+            // 
+            this.ncaracteres.AutoSize = true;
+            this.ncaracteres.Location = new System.Drawing.Point(80, 268);
+            this.ncaracteres.Name = "ncaracteres";
+            this.ncaracteres.Size = new System.Drawing.Size(35, 13);
+            this.ncaracteres.TabIndex = 4;
+            this.ncaracteres.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 268);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Filas:";
+            // 
+            // nfilas
+            // 
+            this.nfilas.AutoSize = true;
+            this.nfilas.Location = new System.Drawing.Point(169, 268);
+            this.nfilas.Name = "nfilas";
+            this.nfilas.Size = new System.Drawing.Size(35, 13);
+            this.nfilas.TabIndex = 6;
+            this.nfilas.Text = "label4";
+            // 
+            // text
+            // 
+            this.text.Location = new System.Drawing.Point(0, 28);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(764, 237);
+            this.text.TabIndex = 7;
+            this.text.Text = "";
+            this.text.TextChanged += new System.EventHandler(this.text_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 325);
+            this.Controls.Add(this.text);
+            this.Controls.Add(this.nfilas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.ncaracteres);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "Form1";
+            this.Text = "Editor de texto con hilos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton tsdtipoletra;
+        private System.Windows.Forms.ToolStripMenuItem wideeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem arialToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox tstamañoletra;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label ncaracteres;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label nfilas;
+        private System.Windows.Forms.RichTextBox text;
+    }
+}
+
---
+++ practica5-Hilo1/practica5-Hilo1/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practica5_Hilo1
+{
+    public partial class Form1 : Form
+    {
+        //
+        private Thread hiloSecundario;
+
+        /** Controladores de eventos de espera para el hilo principal y el hilo secundario */
+        private ManualResetEvent controladorPararHiloSecundario;
+        private ManualResetEvent controladorHiloSecundarioParado;
+
+        //Delegado para asignarle valor a pbProgreso
+        private delegate void SetValueDelegate(int prValue);
+
+        //Objeto para el acceso a los controles desde un hilo secundario
+         MethodInvoker delegado;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //tsdtipoletra.DropDownItems.;
+            
+            tstamañoletra.SelectedIndex = 2;
+            ncaracteres.Text = "0";
+            nfilas.Text = "0";
+            Wide();
+
+
+
+            //Crear los contradores para llevar a cabo la detension de manera ordenada
+            controladorPararHiloSecundario = new ManualResetEvent(false);
+            controladorHiloSecundarioParado = new ManualResetEvent(false);
+
+            //Delegado que hace referencia al método que tiene que ejecutar el hilo
+            ThreadStart delegadoPS = new ThreadStart(TareaSecundaria);
+            //Creación del hilo
+            hiloSecundario = new Thread(delegadoPS);
+            //Ejecución del hilo
+            hiloSecundario.Start();
+        }
+
+        private void wideeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Wide();
+        }
+
+        private void Wide()
+        {
+            int tam = int.Parse(tstamañoletra.SelectedItem.ToString());
+            if (text.SelectionFont != null)
+            {
+                System.Drawing.Font currentFont = text.SelectionFont;
+                System.Drawing.FontStyle newFontStyle;
+
+                if (text.SelectionFont.Bold == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Bold;
+                }
+
+                text.SelectionFont = new Font(
+                   "wide latin",
+                   tam,
+                   newFontStyle
+                );
+                tsdtipoletra.Text = "Wide Lite";
+            }
+        }
+
+        private void arialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // text.SelectionFont = new Font("Arial", 12, FontStyle.Bold);
+            Arial();
+        }
+
+        private void Arial()
+        {
+            int tam = int.Parse(tstamañoletra.SelectedItem.ToString());
+            if (text.SelectionFont != null)
+            {
+                System.Drawing.Font currentFont = text.SelectionFont;
+                System.Drawing.FontStyle newFontStyle;
+
+                if (text.SelectionFont.Bold == true)
+                {
+                    newFontStyle = FontStyle.Regular;
+                }
+                else
+                {
+                    newFontStyle = FontStyle.Bold;
+                }
+
+                text.SelectionFont = new Font(
+                   "Arial",
+                   tam,
+                   newFontStyle
+                );
+                tsdtipoletra.Text = "Arial";
+            }
+        }
+
+        private void Validaciones_tamaño_de_letra()
+        {
+            if (tstamañoletra.SelectedIndex == 0 )
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if ( tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            
+            if (tstamañoletra.SelectedIndex == 1)
+            {
+
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 2)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 3)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 4)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 5)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 6)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 7)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                    Wide();
+                }
+                if (tsdtipoletra.Text == "Arial")
+                {
+                    Arial();
+                }
+            }
+            if (tstamañoletra.SelectedIndex == 8)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                Wide();
+                 }
+                 if (tsdtipoletra.Text == "Arial")
+                 {
+                      Arial();
+                 }
+            }
+            if (tstamañoletra.SelectedIndex == 9)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                Wide();
+                 }
+                 if (tsdtipoletra.Text == "Arial")
+                 {
+                      Arial();
+                 }
+            }
+            if (tstamañoletra.SelectedIndex == 10)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                Wide();
+                
+                 }
+                 if (tsdtipoletra.Text == "Arial")
+                 {
+                      Arial();
+                 }
+            }
+            if (tstamañoletra.SelectedIndex == 11)
+            {
+                if (tsdtipoletra.Text == "Wide Latin")
+                {
+                Wide();
+                 }
+                 if (tsdtipoletra.Text == "Arial")
+                 {
+                      Arial();
+                 }
+            }
+        }
+
+        private void tstamañoletra_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Validaciones_tamaño_de_letra();
+        }
+
+        ///////////////////////////////////////////////
+        ///
+        private void TareaSecundaria()
+        {
+            
+            int caracteres = 0, filas = 0;
+            int hecho = 0, tpHecho = 0;
+            while (true)
+           {
+                //    hecho += 1;
+                //    tpHecho = (int)(hecho / numCargaUCP.Value * 100);
+                //    if (tpHecho > text.Text.Length)
+                //        SetValue_pbProgreso(tpHecho);
+                delegado = new MethodInvoker(SetValue_ncaracteres);
+                ncaracteres.Invoke(delegado);
+                delegado = new MethodInvoker(SetValue_nfilas);
+                nfilas.Invoke(delegado);
+
+                Thread.Sleep(100);
+            }
+
+            
+
+           
+        }
+
+        //Método para asignar valor a pbProgreso desde un delegado
+       private void SetValue_pbProgreso(int hecho)
+        {
+            //Si no estamos en el hilo que creo el objeto pbProgreso necesitamos 
+            //llamar a Invoke por lo que InvikeRequired devolverá true
+            if (text.InvokeRequired)
+           {
+                SetValueDelegate delegado = new SetValueDelegate(SetValue_pbProgreso);
+                text.Invoke(delegado, new object[] { hecho });
+            }
+            else
+                nfilas.Text = Convert.ToString(hecho);
+       }
+
+        //Metodo para acceder a btCalcular desde un MethodInvoker
+        private void SetValue_ncaracteres()
+        {
+
+            if (text.Text != null)
+            {
+                
+                ncaracteres.Text = Convert.ToString(text.Text.Length);
+            }
+            
+        }
+
+        //Metodo para acceder a numCargaUCP desde un MethodInvoker
+        private void SetValue_nfilas()
+        {
+            if (text.Text != null)
+            {
+                nfilas.Text = Convert.ToString(text.Lines.Count());
+               // nfilas.Text = Convert.ToString(text.Text.Length);
+            }
+            
+        }
+
+        private bool pararHiloSecundario()
+        {
+            /* si el hilo no ha sifo creado o si este no esta en ejecución, retornamos false */
+            if (hiloSecundario == null || !hiloSecundario.IsAlive)
+                return false;
+
+            /* Cambiar el estado de espera a señalizado para informar al hilo secundario que debe parar */
+            controladorPararHiloSecundario.Set();
+
+            while (hiloSecundario.IsAlive)
+            {
+                /** Esperamos a que los hilos secundarios informen que han parado. Esta espera se realiza 
+                 * durante 100 milisegundos de no ocurrir ningun aviso se continua **/
+                if (WaitHandle.WaitAll(new WaitHandle[] { controladorHiloSecundarioParado }, 100, true))
+                    break;
+
+                /** Se procesan los eventos que se hayan producido en el tiempo de bloqueo **/
+                Application.DoEvents();
+            }
+
+            return true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pararHiloSecundario())
+                return;
+        }
+
+
+
+
+        /// <summary>
+        /// evento Text change 
+        /// </summary>
+        /// 
+        private void text_TextChanged(object sender, EventArgs e)
+        {
+         //   TareaSecundaria();
+        }
+    }
+}
